9f8e569c86093c1648673bac93ee0f5e1e6e6620
Fazlur Abba <fazlur.abba@demica.com>
2022-12-21 18:00:18 +0000

    Started fixing T16 errors.py

--- T16/Task files/errors.py
+++ T16/Task files/errors.py
@@ -3,19 +3,22 @@
 # There are some errors in this program, try run the program by pressing F5.
 # Now look at the error messages and find and fix the errors.
 
-print "Welcome to the error program"
-    print "\n"
+# Fixed a SyntaxError on line  6, the print was missing the brackets
+print ("Welcome to the error program")
+# Fixed a SyntaxError and IndentationError onpre vious line 7 (code below), the print was missing the brackets and was indented
+print ("\n")
 
-    ageStr == "24 years old" #I'm 24 years old.
-    age = int(ageStr)
-    print("I'm" + age + "years old.")
-    three = "3"
+# The next 6 lines (below) were indented where they didn't have to
+ageStr == "24 years old" #I'm 24 years old.
+age = int(ageStr)
+print("I'm" + age + "years old.")
+three = "3"
 
-    answerYears = age + three
-
-print "The total number of years:" + "answerYears"
+answerYears = age + three
+#The next 2 prints where missing the brackets, which is a syntax error
+print ("The total number of years:" + "answerYears")
 answerMonths = answer * 12
-print "In 3 years and 6 months, I'll be " + answerMonths + " months old"
+print ("In 3 years and 6 months, I'll be " + answerMonths + " months old")
 
 #HINT, 330 months is the correct answer
 
